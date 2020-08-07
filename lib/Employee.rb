7f28b8a8bc492c46bf2f27dabe4bf03170ea1866
anickel101 <anson.nickel@gmail.com>
2020-08-06 23:46:19 -0400

    Completed Lab

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,30 @@
 class Employee
 
+    attr_accessor :name, :salary, :manager_name
+
+    @@all = []
+
+    def initialize(name, salary)
+        @name = name
+        @salary = salary
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(fixnum)
+        self.all.select {|employee| employee.salary > fixnum}
+    end
+
+    def self.find_by_department(dept_name)
+        self.all.find_all {|employee| employee.manager_name.dept === dept_name}
+    end
+
+    def tax_bracket
+        binding.pry
+        Employee.all.select {|employee| employee.salary < (self.salary + 1000) && employee.salary > (self.salary - 1000) }
+    end
+
 end
--- run.rb
+++ run.rb
@@ -3,7 +3,19 @@ require_relative "lib/Employee"
 require 'pry'
 
 
-#Test your code here
+michael = Manager.new("Michael", "Paper", 52)
+jan = Manager.new("Jan", "Rock", 48)
+
+michael.hire_employee("Creed", 50000)
+michael.hire_employee("Ryan", 20000)
+
+dwight = Employee.new("Dwight", 20500)
+dwight.manager_name = michael
+
+erin = Employee.new("Erin", 20500)
+erin.manager_name = michael
+
+
 
 
 binding.pry
